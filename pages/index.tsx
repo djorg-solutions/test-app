ccece4566ce5162210b6f0befcd09741ac07fc6d
djorg-solutions <djorg90@gmail.com>
2022-09-01 01:25:18 -0400

    fix range date input

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,24 @@ import type { NextPage } from 'next';
 import { ButtonGroup, Button, Grid, Box, TextField } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers-pro';
 import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
-import { DateRangePicker, DateRange } from '@mui/x-date-pickers-pro/DateRangePicker';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import TranferList from 'src/components/common/TranferList';
 import ReportButtons from 'src/components/common/ReportButtons';
 import ReportContent from 'src/components/common/ReportContent';
-import {Consultant} from 'src/models/Consultant';
-import {getConsultants} from 'src/services/consultants.service';
-import {useMutation} from 'react-query';
-import {sendConsultants} from 'src/services/consultants.service';
+import { Consultant } from 'src/models/Consultant';
+import { getConsultants } from 'src/services/consultants.service';
+import { useMutation } from 'react-query';
+import { sendConsultants } from 'src/services/consultants.service';
 
 
-interface HomePageProps{
+interface HomePageProps {
   consultants: Consultant[];
 }
 
-const Home = ({consultants}: HomePageProps) => {
+const Home = ({ consultants }: HomePageProps) => {
 
-  const [rangeDate, setRangeDate] = useState<DateRange<Dayjs>>([null, null]);
+  const [startDate, setStartDate] = useState<Dayjs>([null, null]);
+  const [endDate, setEndDate] = useState<Dayjs>([null, null]);
   const [tabSelected, setTabSelected] = useState(0);
   const [reportTypeSelected, setReportTypeSelected] = useState(-1);
 
@@ -27,51 +28,66 @@ const Home = ({consultants}: HomePageProps) => {
 
   const { mutate: send } = useMutation(sendConsultants, {
     onSuccess: (data) => {
-        
+
     },
     onError: (error) => {
-        alert('An error occurred on the server!');
+      alert('An error occurred on the server!');
     }
-});
+  });
+
+  const handleStarDate = (newValue: Dayjs | null) => {
+    setStartDate(newValue);
+  };
+
+  const handleEndDate = (newValue: Dayjs | null) => {
+    setEndDate(newValue);
+  };
 
   const handleReportType = value => {
-    if (consultantsSelected.length > 0 && rangeDate[0]) {
+    if (consultantsSelected.length > 0 && startDate.$d && endDate.$d) {
       setReportTypeSelected(value);
       send({
         consultants: consultantsSelected,
         reportType: value,
-        startDate: new Date(rangeDate[0].$d),
-        endDate: new Date(rangeDate[1].$d)
+        startDate: new Date(startDate.$d),
+        endDate: new Date(endDate.$d)
       })
     }
-  } 
+  }
 
   return (
     <Grid container>
       <Grid xs={12} item>
         <Grid container justifyContent={'space-between'}>
-          <ButtonGroup size="small" aria-label="small button group">
-            <Button onClick={() => setTabSelected(0)} variant={tabSelected === 0 ? 'contained' : 'outlined'}>{'Por consultor'}</Button>
-            <Button onClick={() => setTabSelected(1)} variant={tabSelected === 1 ? 'contained' : 'outlined'}>{'Por cliente'}</Button>
-          </ButtonGroup>
+          <Grid item>
+            <ButtonGroup size="small" aria-label="small button group">
+              <Button onClick={() => setTabSelected(0)} variant={tabSelected === 0 ? 'contained' : 'outlined'}>{'Por consultor'}</Button>
+              <Button onClick={() => setTabSelected(1)} variant={tabSelected === 1 ? 'contained' : 'outlined'}>{'Por cliente'}</Button>
+            </ButtonGroup>
+          </Grid>
           <LocalizationProvider
             dateAdapter={AdapterDayjs}
             localeText={{ start: 'Desde', end: 'Hasta' }}
           >
-            <DateRangePicker
-              value={rangeDate}
-              inputFormat={'DD/MM/YYYY'}
-              onChange={(newValue) => {
-                setRangeDate(newValue);
-              }}
-              renderInput={(startProps, endProps) => (
-                <>
-                  <TextField {...startProps} size={'small'} />
-                  <Box sx={{ mx: 2 }}> a </Box>
-                  <TextField {...endProps} size={'small'} />
-                </>
-              )}
-            />
+            <Grid item>
+              <DesktopDatePicker
+                label="Começo"
+                views={['year', 'month']}
+                inputFormat={'MM/YYYY'}
+                value={startDate}
+                onChange={handleStarDate}
+                renderInput={(params) => <TextField {...params} size={'small'} />}
+              />
+              <DesktopDatePicker
+                views={['year', 'month']}
+                label="Finalizar"
+                inputFormat={'MM/YYYY'}
+                value={endDate}
+                onChange={handleEndDate}
+                renderInput={(params) => <TextField {...params} size={'small'} />}
+              />
+            </Grid>
+
           </LocalizationProvider>
         </Grid>
       </Grid>
@@ -92,7 +108,7 @@ const Home = ({consultants}: HomePageProps) => {
       </Grid>
 
       <Grid item xs={12} mt={4}>
-          <ReportContent selected={reportTypeSelected} />      
+        <ReportContent selected={reportTypeSelected} />
       </Grid>
     </Grid>
   );
@@ -102,7 +118,7 @@ export async function getServerSideProps() {
 
   let consultants: Consultant[] = await getConsultants();
 
-  return { props: {consultants} }
+  return { props: { consultants } }
 }
 
 export default Home;
