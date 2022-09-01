8a82ec6ba5bdd3955e788764754127e4ef139604
djorg-solutions <djorg90@gmail.com>
2022-08-31 22:03:30 -0400

    layout done

--- src/components/common/ReportContent/GraficoReportType/index.tsx
+++ src/components/common/ReportContent/GraficoReportType/index.tsx
@@ -1,10 +1,64 @@
 import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
 
 const GraficoReportType = () => {
+    
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        BarElement,
+        Title,
+        Tooltip,
+        Legend
+      );
+      
+    const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top' as const,
+          },
+          title: {
+            display: true,
+            text: 'Gráfico',
+          },
+        },
+      };
+      
+         
+      const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+      
+      const data = {
+        labels,
+        datasets: [
+          {
+            label: 'Dataset 1',
+            data: [2,3,7,11,20,15,13],
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          },
+          {
+            label: 'Dataset 2',
+            data: [12,13,4,15,10,1,9],
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+          },
+        ],
+      };
+
     return (
-        <div>
-            G
-        </div>
+        <>
+           <Bar options={options} data={data} />
+        </>
     );
 };
 
